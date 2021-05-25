1a46f4fbe973cbabea6f7ef246ee5a89903e35d6
pedin <pedin@DESKTOP-BC6CV40>
2021-05-25 10:30:18 -0400

    Adding NewFile

---
+++ TestNGDemo/src/com/TestNGPackage/AddingNewFile.java
@@ -0,0 +1,13 @@
+package com.TestNGPackage;
+
+import org.testng.annotations.Test;
+
+public class AddingNewFile {
+	
+	@Test
+	public void helloworldtest()
+	{
+		System.out.println("helloworld");
+	}
+
+}
